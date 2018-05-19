07712017803e200dbbc44156c444f60ced5122ae
farhan <farhan1@live.co.uk>
2018-05-19 20:47:28 +0100

    implenting react-navigation and passing parametres to routes Signed-off-by: farhan <farhan1@live.co.uk>

--- App.js
+++ App.js
@@ -4,25 +4,59 @@
  * @flow
  */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    titles: 'QuestionSwap',
+    };
+    }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Awesome Project Home Screen</Text>
+        <Button
+         title="Go to Instructions"
+         onPress={() => {
+         this.props.navigation.navigate('Instructions',{
+          itemID: 86,
+          otherParam: 'anything',
+         });
+         }}
+       />
       </View>
     );
   }
 }
 
 
-  class DetailsScreen extends React.Component {
+  class InstructionsScreen extends React.Component {
     render(){
+      const { navigation } = this.props;
+      const itemId = navigation.getParam('itemId', 'NO-ID');
+      const otherParam = navigation.getParam('otherParam', 'some default value');
+
       return (
-        <View styles ={{ flex:1, alighnItems: 'center', justifyContent: 'center'}}>
+        <View styles ={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
           <Text>Instructions</Text>
+          <Text>ItemId: {JSON.stringify(itemId)}</Text>
+          <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+          <Button
+          title="Go to Instructions again"
+          onPress={() =>
+          this.props.navigation.push('Instructions', {
+          itemId: Math.floor(Math.random() * 100),
+          })}
+          />
+          <Button
+            title="go home"
+            onPress={() => this.props.navigation.navigate('Home')}
+            />
+          <Button
+            title = "Go Back"
+            onPress={() => this.props.navigation.goBack()}
+            />
         </View>
         );
      }
@@ -31,7 +65,7 @@ class HomeScreen extends React.Component {
     const RootStack = createStackNavigator(
       {
         Home: HomeScreen,
-        Details: DetailsScreen,
+        Instructions: InstructionsScreen,
         },
         {
          initialRoutneName: 'Home',
